fc5ba387cd9fb84909185f88859302738e15ef6e
jackklop <jackklop123@gmail.com>
2020-03-02 19:25:41 -0500

    Updates

---
+++ precache-manifest.a4684bf8ac2923672e81a9c8b2eb576d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a404818c3916e12c2ab5a6fd268d05fc",
+    "url": "/cheezyjokes/index.html"
+  },
+  {
+    "revision": "9930e42ca166ccae09f4",
+    "url": "/cheezyjokes/static/css/main.ca662e14.chunk.css"
+  },
+  {
+    "revision": "771e67e56ec4dec655a6",
+    "url": "/cheezyjokes/static/js/2.42d1fe91.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/cheezyjokes/static/js/2.42d1fe91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9930e42ca166ccae09f4",
+    "url": "/cheezyjokes/static/js/main.d9fb2459.chunk.js"
+  },
+  {
+    "revision": "41399d30aa3b5e1e7c5f",
+    "url": "/cheezyjokes/static/js/runtime-main.c8e2e70f.js"
+  },
+  {
+    "revision": "9d66903858f34cf0b561faf5834a12c3",
+    "url": "/cheezyjokes/static/media/cheese.9d669038.png"
+  }
+]);
